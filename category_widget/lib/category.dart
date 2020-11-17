0a2e2acb6f27fb11a1e135cedea08d10bad885db
Artem Ulko <ulkoart@gmail.com>
2020-11-17 14:30:32 +0300

    category

---
+++ category_widget/lib/category.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
+
+void printTapped(String aText) {
+  print('$aText.');
+}
+
+class Category extends StatelessWidget {
+  final String name;
+  final ColorSwatch color;
+  final IconData icon;
+
+  const Category({
+    Key key,
+    @required this.name,
+    @required this.color,
+    @required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: color,
+          splashColor: color,
+          onTap: () {
+            printTapped('tapped');
+          },
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    icon,
+                    size: 60.0,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
