13fb159c6acf11aee3a1a99a1c902663164db518
ulkoart <ulkoart@gmail.com>
2020-12-06 16:22:43 +0300

    upd

--- unit_converter/lib/main.dart
+++ unit_converter/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:unit_converter/category_route.dart';
 
+import 'category_route.dart';
 
 void main() {
   runApp(UnitConverterApp());
 }
-
 class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
+      ),
       home: CategoryRoute(),
     );
   }
